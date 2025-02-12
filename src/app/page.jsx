db7f0444ddde455cf9803afda4162202c064bcdc
sistema2a <sistemafinanciero2a@gmail.com>
2025-02-12 16:24:11 -0400

    Agregando estructura nueva

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div>Pagina Principal</div>
+  )
+}
+
+export default Home
---
+++ src/libs/db.js
@@ -0,0 +1,9 @@
+import mariadb from 'mariadb';
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
